10a1498d7cf81ee4e8b1394b902e2ef4a6e4793b
kleenkanteen <antpalmolive@gmail.com>
2024-04-15 23:32:59 -0400

    chore: env stuff

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -1,10 +1,23 @@
 import nextcord
 from nextcord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv("TOKEN")
 
 bot = commands.Bot()
 
-@bot.slash_command(description="Replies with pong!")
-async def ping(interaction: nextcord.Interaction):
-    await interaction.send("Pong!", ephemeral=True)
+intents = nextcord.Intents.default()
+intents.messages = True # Enable the messages intent
 
-bot.run("token")
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_message(message):
+    # Check if the message is a DM
+    if isinstance(message.channel, nextcord.DMChannel):
+        # Reply to the DM
+        await message.reply('This is a reply to your DM!')
+
+bot.run(token)
